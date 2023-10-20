df78900af3137c8b31f792aab5ce6bf88aed258f
Momore <momoreoke@gmail.com>
2023-10-20 11:50:38 +0000

    init

---
+++ LIbraryManagement/Controllers/BookController.cs
@@ -0,0 +1,44 @@
+﻿using LIbraryManagement.Data;
+using LIbraryManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LIbraryManagement.Controllers
+{
+    public class BookController : Controller
+    {
+        ApplicationDbContext _ctx;
+        public BookController(ApplicationDbContext ctx) 
+        {
+            _ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            List<Book> books = _ctx.Books.ToList();
+            return View(books);
+        }
+
+        public IActionResult Add() 
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Book book) 
+        {
+            _ctx.Books.Add(book);
+            _ctx.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id) 
+        {
+            Book book = _ctx.Books.Find(id);
+            _ctx.Books.Remove(book);
+            _ctx.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
---
+++ LIbraryManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LIbraryManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LIbraryManagement.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
